0af38891c78d4682f6beaa521ff95fe337bc9f16
tryta <alexandere@getacademy.no>
2019-04-04 10:39:30 +0200

    oppdatering

---
+++ js/lagNyttMål.js
@@ -0,0 +1,26 @@
+﻿var mainGoal;
+
+function newGoal() {}
+
+function lagNyttMål() {
+    header.innerHTML = '<h1>Nytt Mål</h1>';
+    content.classList.length = 0;
+    content.classList.add('myGrid');
+    content.classList.add('lagNyttMål');
+    content.innerHTML = `
+    <div style="align-self: center">
+        Opprett et nytt mål:
+    </div>
+    <div><textarea class="mainGoalInput"></textarea></div>
+    <div><button onclick="showMainScreen()">Tilbake</button> <button onclick="showMainScreen()">Lagre og ferdig</button> <button>Nytt mål</button</div>
+    `;
+}
+
+function mainextra() {
+    var mainGoalInput = document.getElementById('mainGoalInput');
+    mainGoal = mainGoalInput.value;
+
+    mainextra.innerHTML = `
+                <div class="overskrift back" onclick="newGoal()">←</div>
+                Du har valgt dette som ditt mål: ${mainGoal}`;
+}
