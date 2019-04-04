0439291cf01d2e5cb9c5d57b0ef28ca6a4c1831d
tryta <47241882+tryta@users.noreply.github.com>
2019-04-04 10:20:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+﻿function showMainScreen() {
+    var mainGoalTxt = mainGoal;
+    if (!mainGoal) {
+        mainGoalTxt = '<i>Velg "<b>Nytt mål</b>" for å legge inn ditt mål</i>';
+    }
+    header.innerHTML = '<h1>Hovedside</h1>';
+    content.setAttribute('class', 'mainPageContentGrid');
+    content.innerHTML = `
+    <button class="mainGoals" onclick="showMyGoalsScreen()">
+              ${mainGoalTxt}     
+    </button>
+      <div><button class="mainButton" onclick="ukentligEvaluering()">Ukentlig evaluering</button></div>
+      <div><button class="mainButton" onclick="showEvaluateScreen()">Daglig evaluering</button></div>
+      <div><button class="mainButton" onclick="lagNyttMål()">Nytt mål</button></div>
+      <div><button class="mainButton" onclick="lagDagbok()">Dagbok</button></div>
+      <div><button class="mainButton" onclick="kalender()">Kalender</button></div>
+    `;
+}
